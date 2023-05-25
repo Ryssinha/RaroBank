e4d09715dad1b72577ba8b79d282286db9fa45cd
Rayssa Guilherme <rayssasoares161@gmail.com>
2023-05-25 15:11:50 -0300

    feat: add method show,edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,18 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show edit update destroy ]
 
   def index
     @users = User.all
   end
 
+  def show
+  end
+
   def new
     @user = User.new
+  end
+
+  def edit
   end
 
   def create
@@ -34,6 +41,7 @@ class UsersController < ApplicationController
     end
   end
 
+
   def destroy
     @user.destroy
 
@@ -43,5 +51,12 @@ class UsersController < ApplicationController
     end
   end
 
+  private
 
-end
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :cpf)
+    end
