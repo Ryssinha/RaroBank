4a947d90e73329e9b30557f21da9e1807edec9fc
Rayssa Guilherme <rayssasoares161@gmail.com>
2023-05-29 23:18:41 -0300

    feat:populating seed

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
   factory :user do
     name { Faker::Name.name }
     cpf { Faker::IDNumber.brazilian_citizen_number }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password(min_length: 6) }
   end
 end
