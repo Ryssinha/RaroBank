637af0aa3c8ad6f12e819f4f201cf1889fef1456
Rayssa Guilherme <rayssasoares161@gmail.com>
2023-05-29 22:37:32 -0300

    feat: add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
-# frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users, controllers: { confirmations: 'confirmations' }
   root 'home#index'
-
-  resources :users
+  
 end
