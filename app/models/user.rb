9c836b3125daf45c793312bd633f8c2286ef87c5
Rayssa Guilherme <rayssasoares161@gmail.com>
2023-05-29 23:07:33 -0300

    feat: model devise user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   validates :name, presence: true
   validates :cpf, presence: true, uniqueness: true
+  validates :cpf, length: { is: 11 }
 end
