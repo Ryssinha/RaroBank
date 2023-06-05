acee40df0dea67845d63eada4d7f96659f1851b1
Rayssa Guilherme <rayssasoares161@gmail.com>
2023-06-04 23:33:03 -0300

    feat:create model balance and relation user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  belong_to :administrator
+  has_one :balance
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   validates :name, presence: true
