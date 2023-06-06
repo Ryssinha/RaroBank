245da6a5e8b1112bf1025c78364a449774cb562b
Cristiano dos Santos <cristianosql02@gmail.com>
2023-06-06 02:47:22 -0400

    feat: first step of validations for applied migration

--- db/migrate/20230606063810_create_products.rb
+++ db/migrate/20230606063810_create_products.rb
@@ -1,14 +1,14 @@
 class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
-      t.string :name
-      t.date :start_date
-      t.date :end_of_term
-      t.decimal :minimum_investment_amount
-      t.string :index
+      t.string :name, null: false, length: { minimum: 2, maximum: 100 }
+      t.integer :punctuation
+      t.date :start_date, null: false
+      t.date :end_of_term, null: false
+      t.decimal :minimum_investment_amount, null: false
+      t.string :index, null: false
 
       t.timestamps
     end
-    add_index :products, :minimum_investment_amount
   end
 end
