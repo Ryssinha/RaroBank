a222fb427a619a5d5ffb89bc02102d5ac11c02e1
Cristiano dos Santos <cristianosql02@gmail.com>
2023-06-06 02:23:14 -0400

    feat: upload route creation and assignment

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { confirmations: 'confirmations' }
   root 'home#index'
-
+  post '/upload_image', to: 'home#upload_image', as: 'upload_image'
+  
   resources :classrooms
 end
 
