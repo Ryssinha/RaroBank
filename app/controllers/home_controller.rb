37fbbe60f2322292dddfe9a7891487c97d3e80c1
Cristiano dos Santos <cristianosql02@gmail.com>
2023-06-06 02:22:10 -0400

    test: calling the function inside the controller only for debugging and testing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,12 @@ class HomeController < ApplicationController
     data = JSON.parse(response.body)
     @selic = data.last['valor']  
   end
+
+  def upload_image
+    image = params[:image]
+    response = ImgbbService.upload_image(IMGBB_KEY, image)
+    
+    puts JSON.parse(response)
+    data = JSON.parse(response)
+  end  
 end
