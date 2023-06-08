14286750766c6daf318a2eeeb9a77c8c5f8d758e
Cristiano dos Santos <cristianosql02@gmail.com>
2023-06-08 00:07:35 -0400

    feat: creation of the rate model along with the respective validations at the application level

---
+++ app/models/fee.rb
@@ -0,0 +1,5 @@
+class Fee < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 100 }, uniqueness: true
+  validates :value, presence: true, inclusion: { in: 0..100, message: "Deve estar entre 0 e 1000" }
+  validates :latest_release, presence: true
+end
