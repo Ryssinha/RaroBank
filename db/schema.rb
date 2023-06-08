004eff58441c2f887ffc76d660d8ca8df069f058
Cristiano dos Santos <cristianosql02@gmail.com>
2023-06-08 00:08:10 -0400

    feat: automatic generation of new rate table in the database

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,16 @@
-ActiveRecord::Schema[7.0].define(version: 2023_06_03_161341) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_06_08_032122) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +33,25 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_03_161341) do
 
   create_table "classrooms", force: :cascade do |t|
     t.string "name", limit: 255, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "fees", force: :cascade do |t|
+    t.string "name", limit: 100, null: false
+    t.float "value", null: false
+    t.date "latest_release", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "punctuation"
+    t.date "start_date", null: false
+    t.date "end_of_term", null: false
+    t.decimal "minimum_investment_amount", null: false
+    t.string "index", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
