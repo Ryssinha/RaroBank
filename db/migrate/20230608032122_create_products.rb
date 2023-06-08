181dd81cadebcea0d49d0867372982abbb96005c
Cristiano dos Santos <cristianosql02@gmail.com>
2023-06-08 02:53:11 -0400

    featchage fees and products table for creating order

--- db/migrate/20230608032122_create_products.rb
+++ db/migrate/20230608032122_create_products.rb
@@ -1,12 +1,13 @@
 class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
-      t.string :name, null: false, length: { minimum: 2, maximum: 100 }
+      t.string :name, null: false, unique: true, length: { minimum: 4, maximum: 100 }
       t.integer :punctuation
       t.date :start_date, null: false
       t.date :end_of_term, null: false
       t.decimal :minimum_investment_amount, null: false
-      t.string :index, null: false
+      t.references :fee, foreign_key: true
+      t.string :image_url, null: false
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,8 @@ json_indexers = File.read(Rails.root.join('db', 'indexers.json'))
 indexers = JSON.parse(json_indexers)['indexers']
 
 puts "Iniciando seed"
-
+# Product.create(name: "Teste", punctuation: 1, start_date: Date.current, end_of_term: 6.months.since(Date.current), minimum_investment_amount: 10.00, index: Fee.find_by_name("SELIC").value)
+    
 indexers.each do |indexer|
     data = Indexers.send(indexer['method'])
   
