4621fbcf3717dc30c1223efcf60fdfd2c1122420
Cristiano dos Santos <cristianosql02@gmail.com>
2023-06-08 21:32:14 -0400

    refactor: implementing improvement in migration

--- db/migrate/20230608032122_create_products.rb
+++ db/migrate/20230608032122_create_products.rb
@@ -1,13 +1,15 @@
 class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
-      t.string :name, null: false, unique: true, length: { minimum: 4, maximum: 100 }
-      t.integer :punctuation
-      t.date :start_date, null: false
+      t.string :name, null: false, unique: true, length: { minimum: 4, maximum: 20 }
+      t.integer :punctuation, default: 0
+      t.date :start_date, default: -> { "CURRENT_DATE" }
       t.date :end_of_term, null: false
       t.decimal :minimum_investment_amount, null: false
-      t.references :fee, foreign_key: true
       t.string :image_url, null: false
+      t.boolean :premium, default: false
+      t.float :additional_fee, null: false
+      t.references :fee, foreign_key: true
 
       t.timestamps
     end
