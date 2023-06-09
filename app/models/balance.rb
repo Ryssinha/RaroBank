2ef75f4968b40dd6f594cc78350b52bc32bf81db
Rayssa Guilherme <rayssasoares161@gmail.com>
2023-06-08 22:28:04 -0300

    fix:refactoring user model

--- app/models/balance.rb
+++ app/models/balance.rb
@@ -13,4 +13,20 @@ class Balance < ApplicationRecord
   def calculate_current_balance
     self.current_balance = balance_deposited - withdrawn
   end
+
+  def withdraw(amount)
+    self.current_balance ||= 0.0
+    self.current_balance -= amount
+    self.withdrawn ||= 0.0
+    self.withdrawn += amount
+    save!
+  end
+
+  def deposit(amount)
+    self.current_balance ||= 0.0
+    self.current_balance += amount
+    self.balance_deposited ||= 0.0
+    self.balance_deposited += amount
+    save!
+  end
 end
