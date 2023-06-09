6f200aa6e303312cc1df73242c9c754350ec74f0
Rayssa Guilherme <rayssasoares161@gmail.com>
2023-06-08 22:29:35 -0300

    fix:refactoring user controller

--- app/controllers/balances_controller.rb
+++ app/controllers/balances_controller.rb
@@ -9,10 +9,10 @@ class BalancesController < ApplicationController
       @balance_deposited = @balance.balance_deposited
       @withdrawn = @balance.withdrawn
     else
-      format.html { redirect_to root_path, alert: 'Você não tem permissão para visualizar esse saldo.' }
+      redirect_to root_path, alert: 'Você não tem permissão para visualizar esse saldo.'
     end
   rescue ActiveRecord::RecordNotFound
-    format.html { redirect_to root_path, alert: 'Saldo não encontrado.' }
+    redirect_to root_path, alert: 'Saldo não encontrado.'
   end
 
   def deposit
@@ -21,12 +21,15 @@ class BalancesController < ApplicationController
 
     if @balance.user == current_user && amount.positive?
       @balance.deposit(amount)
-      format.html { redirect_to @balance, notice: "Saldo atualizado com sucesso. Saldo atual: #{'%.2f' % @balance.current_balance}" }
+
+      @current_balance = @balance.current_balance
+      @balance_deposited = @balance.balance_deposited
+      @withdrawn = @balance.withdrawn
+
+      redirect_to @balance, notice: "Saldo atualizado com sucesso. Saldo atual: #{'%.2f' % @current_balance}"
     else
-      format.html { redirect_to @balance, alert: 'Falha ao adicionar saldo. Certifique-se de fornecer um valor positivo.' }
+      redirect_to @balance, alert: 'Falha ao adicionar saldo. Certifique-se de fornecer um valor positivo.'
     end
-  rescue ActiveRecord::RecordNotFound
-    format.html {  redirect_to root_path, alert: 'Saldo não encontrado.' }
   end
 
   def withdraw
@@ -35,14 +38,17 @@ class BalancesController < ApplicationController
 
     if @balance.user == current_user && amount.positive? && amount <= @balance.current_balance
       @balance.withdraw(amount)
-      format.html { redirect_to @balance, notice: "Saque realizado com sucesso. Saldo atual: #{'%.2f' % @balance.current_balance}" }
+
+      @current_balance = @balance.current_balance
+      @balance_deposited = @balance.balance_deposited
+      @withdrawn = @balance.withdrawn
+
+      redirect_to @balance, notice: "Saque realizado com sucesso. Saldo atual: #{'%.2f' % @current_balance}"
     elsif @balance.user == current_user && amount > @balance.current_balance
-      format.html { redirect_to @balance, alert: 'Valor do saque excede o saldo atual.' }
+      redirect_to @balance, alert: 'Valor do saque excede o saldo atual.'
     else
-      format.html { redirect_to @balance, alert: 'Falha ao efetuar o saque. Certifique-se de fornecer um valor positivo e menor ou igual ao saldo atual.' }
+      redirect_to @balance, alert: 'Falha ao efetuar o saque. Certifique-se de fornecer um valor positivo e menor ou igual ao saldo atual.'
     end
-  rescue ActiveRecord::RecordNotFound
-    format.html { redirect_to root_path, alert: 'Saldo não encontrado.' }
   end
 
   def current_balance
@@ -53,7 +59,7 @@ class BalancesController < ApplicationController
 
   def check_administrator
     unless current_user.administrator?
-      format.html { redirect_to root_path, alert: 'Você não tem permissão para adicionar saldo.' }
+      redirect_to root_path, alert: 'Você não tem permissão para adicionar saldo.'
     end
   end
 end
