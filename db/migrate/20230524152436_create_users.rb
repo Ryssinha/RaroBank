3394434f1bcf136b3979385319c7dcf12c1e5018
Rayssa Guilherme <rayssasoares161@gmail.com>
2023-06-08 22:42:14 -0300

    feat:migration transfer

--- db/migrate/20230524152436_create_users.rb
+++ db/migrate/20230524152436_create_users.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name, null: false, limit: 255
-      t.string :cpf, null: false, limit: 11
+      t.string :cpf, null: false, limit: 11, unique: true
 
       t.timestamps
     end
