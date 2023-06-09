d163d28d4da89fbc3a0c9b4a2371507ff2b84933
Cristiano dos Santos <cristianosql02@gmail.com>
2023-06-09 12:39:02 -0400

    refactor: callback removal

--- app/models/fee.rb
+++ app/models/fee.rb
@@ -4,15 +4,4 @@ class Fee < ApplicationRecord
   validates :name, presence: true, length: { maximum: 100 }, uniqueness: true
   validates :value, presence: true, inclusion: { in: 0..100, message: "Deve estar entre 0 e 1000" }
   validates :latest_release, presence: true
-
-  before_save :check_duplicate_name
-
-  private
-
-  def check_duplicate_name
-    if Fee.exists?(name: name)
-      errors.add(:name, "já está cadastrado")
-      throw(:abort)
-    end
-  end
 end
