c5890d426654d0e06e74cac03209e1db0f09be95
Rayssa Guilherme <rayssasoares161@gmail.com>
2023-06-08 22:46:10 -0300

    feat:model transfer

---
+++ app/models/transfer.rb
@@ -0,0 +1,39 @@
+class Transfer < ApplicationRecord
+  belongs_to :sender, class_name: "User"
+  belongs_to :receiver, class_name: "User"
+
+  enum :status, {
+    pending: 1,
+    confirmed: 2
+  }, scopes: true, default: :pending
+
+  validates :amount, presence: true, numericality: { greater_than: 0 }
+  validates :sender, presence: true
+  validate :sufficient_balance, if: :sender
+
+  before_create :generate_token
+
+  def execute_transfer
+    sender_balance = sender.balance || Balance.new(user: sender)
+    receiver_balance = receiver.balance || Balance.new(user: receiver)
+
+    Transfer.transaction do
+      sender_balance.withdraw(amount)
+      receiver_balance.deposit(amount)
+    end
+  end
+
+  private
+
+  def generate_token
+    self.token = rand(100000..999999)
+  end
+
+  def sufficient_balance
+    sender_balance = sender.balance || Balance.new(user: sender)
+
+    if amount > sender_balance.current_balance
+      errors.add(:amount, "Saldo insuficiente para realizar a transferência.")
+    end
+  end
+end
