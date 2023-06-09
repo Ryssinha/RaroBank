d65f871b509df092829c0cd5537400c237ef1e59
Cristiano dos Santos <cristianosql02@gmail.com>
2023-06-08 21:31:46 -0400

    feat: new validations in the model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,9 @@ class Product < ApplicationRecord
     belongs_to :fee
     
     validates :name, presence: true, uniqueness: true, length: { minimum: 4, maximum: 100 }
-    validates :start_date, :end_of_term, presence: true
+    validates :end_of_term, presence: true
+    validates :premium, inclusion: { in: [true, false] }
+    validates :additional_fee, presence: true
     validates :minimum_investment_amount, presence: true
-    validates :index, presence: true
+    validates :fee, presence: true
   end
