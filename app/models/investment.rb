2268140d9e154f3410ffa12a0eb280894f51b60a
Cristiano dos Santos <cristianosql02@gmail.com>
2023-06-10 11:25:24 -0400

    feat: changing the callback method of the investment model

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -5,11 +5,12 @@ class Investment < ApplicationRecord
     validates :user, presence: true
     validates :product, presence: true
 
-    before_save :calculate_value_to_redeem
+    after_save :update_value_to_redeem
 
     private
-  
-    def calculate_value_to_redeem
-      self.value_to_redeem = invested_amount * 1.1
+
+    def update_value_to_redeem
+      fee_value = product.fee.value
+      self.update_column(:value_to_redeem, invested_amount * (fee_value / 100))
     end
 end
