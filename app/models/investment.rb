ec54da73da6b3eb8ed66e30ca7b29710e63a6f0f
Cristiano dos Santos <cristianosql02@gmail.com>
2023-06-10 12:59:02 -0400

    refactor: removing callback from tests

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -4,13 +4,4 @@ class Investment < ApplicationRecord
   
     validates :user, presence: true
     validates :product, presence: true
-
-    after_save :update_value_to_redeem
-
-    private
-
-    def update_value_to_redeem
-      fee_value = product.fee.value
-      self.update_column(:value_to_redeem, invested_amount * (fee_value / 100))
-    end
 end
