fcbe06266864b4e508aec1c053b66406b9f478c4
Cristiano dos Santos <cristianosql02@gmail.com>
2023-06-10 16:12:39 -0400

    feat: routing the page and structuring it with v1 product cards

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   devise_for :users, controllers: { confirmations: 'confirmations' }
-  root 'home#index'
+  root to: redirect('/products')
+  get '/products', to: 'products#index'
+
   post '/upload_image', to: 'home#upload_image', as: 'upload_image'
   
   resources :classrooms
