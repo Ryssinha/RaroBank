8a3de7b73ba91ee01dfc8522d8e4f934e5299350
Rayssa Guilherme <rayssasoares161@gmail.com>
2023-06-12 12:29:36 -0300

    fix:translate devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_12_042204) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_12_131847) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -100,6 +100,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_12_042204) do
     t.integer "status", default: 1, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "expires_at"
     t.index ["receiver_id"], name: "index_transfers_on_receiver_id"
     t.index ["sender_id"], name: "index_transfers_on_sender_id"
   end
