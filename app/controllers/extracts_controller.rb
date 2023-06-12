b0e60ff6d19a31398f5912bd5ad1a47d20089c54
Rayssa Guilherme <rayssasoares161@gmail.com>
2023-06-12 16:46:30 -0300

    fix:extract controller

--- app/controllers/extracts_controller.rb
+++ app/controllers/extracts_controller.rb
@@ -2,8 +2,15 @@ class ExtractsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @user = current_user
-    @sent_transfers = @user.sent_transfers.includes(:receiver)
-    @received_transfers = @user.received_transfers.includes(:sender)
+    @user = User.find(params[:user_id])
+
+    if @user == current_user
+      @sent_transfers = @user.sent_transfers.includes(:receiver)
+      @received_transfers = @user.received_transfers.includes(:sender)
+      @investments = @user.investments.includes(:product)
+    else
+      flash[:alert] = "Você não tem permissão para visualizar o extrato desse usuário."
+      redirect_to root_path
+    end
   end
 end
